58088e2e0f758c48b8555a8cfd9fc6667ebb3751
shafe <alam.shafee@gmail.com>
2026-02-25 23:30:25 +0600

    added viewModel

--- app/src/main/java/com/example/orderplacement/OrderViewModel.kt
+++ app/src/main/java/com/example/orderplacement/OrderViewModel.kt
@@ -4,5 +4,30 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 
 class OrderViewModel(application: Application): AndroidViewModel(application)  {
+    private val repository: OrderRepository
+
+    init {
+        val dao = AppDatabase.getDatabase(application).orderDao()
+        repository = OrderRepository(dao)
+        getAllOrderFromViewModel()
+    }
+
+    fun getAllOrderFromViewModel():List<Order> {
+        return repository.getAllOrdersRepo()
+    }
+
+    fun insertOrderFromViewModel(order:Order) {
+        repository.insertOrderRepo(order)
+    }
+
+    fun updateOrderFromViewModel(order:Order) {
+        repository.updateOrderRepo(order)
+    }
+
+    fun deleteOrderFromViewModel(order:Order) {
+        repository.deleteOrderRepo(order)
+    }
+
+
 
 }
